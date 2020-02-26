7c0febbf5658d4e4412a5c9098f9e998777e65ba
Edward Ditler <edward.ditler@me.com>
2020-02-26 10:49:12 +0100

    fixes

--- clean_trajectory.bash
+++ clean_trajectory.bash
@@ -8,6 +8,7 @@ if [[ $nl -eq 1 ]]; then
 elif [[ $nl -eq 2 ]]; then
   for l in $lengths; do
     n=$(grep -P -n "^[^=]    $l" $1 | tail -n1 | cut -f1 -d:)
+    echo "writing ${1}_cleaned.xyz";
     tail -n +$(echo "$n+$l+2" | bc -l) $1 > ${1}_cleaned.xyz;
     break;
   done
--- restart_geoopt.bash
+++ restart_geoopt.bash
@@ -6,10 +6,12 @@ echo $backto;
 for theoutput in "$@"; do
     if grep -q 'GEO run terminated' $theoutput; then
         echo $calc;
-        cd $(dirname $calc);
+        cd $(dirname $theoutput);
         cp -v inp old_inp;
+        mv -v out old_out;
         mv -v zirc-1.restart inp;
-        sbatch run*.bash;
+        cp -v ~/run_gpu.bash .
+        sbatch run_gpu.bash;
         cd $backto;
     fi
 done
