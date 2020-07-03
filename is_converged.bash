e702f9356e3f6a0d8f4b42f706afd25192136fe9
Edward Ditler <edward.ditler@me.com>
2020-07-03 08:56:46 +0200

    Fixes

--- is_converged.bash
+++ is_converged.bash
@@ -4,4 +4,7 @@ for theoutput in "$@"; do
     if grep -q 'exceeded requested execution time' $theoutput; then
         echo "$theoutput exceeded requested execution time";
     fi
+    if grep -q 'MAXIMUM NUMBER OF OPTIMIZATION STEPS REACHED' $theoutput; then
+        echo "$theoutput exceeded number of optimization steps";
+    fi
 done
